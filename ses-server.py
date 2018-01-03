90706d0af34df6884b0bfd2ae574ab5d6fdaf5be
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-03 17:30:03 +0000

    Add notes about SES API

---
+++ ses-server.py
@@ -0,0 +1,9 @@
+
+
+# See AWS API: https://docs.aws.amazon.com/ses/latest/DeveloperGuide/query-interface-requests.html
+
+# This stub should be able to receive Action=SendEmail calls and should store the messages as files on the file system
+
+# There should also be an endpoint that allows the retrieval of the latest N messages
+
+print "TODO"
