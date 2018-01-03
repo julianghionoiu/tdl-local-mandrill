e0fabcaf91cb86d5d29e41720f53916176f5d123
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-03 16:38:03 +0000

    Update the comments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tdl-local-mandrill
-A Mandrill Server Stub that stores the last 10 messages
+
+A Mandrill Server Stub that stores the messages
---
+++ mandrill-server.py
@@ -0,0 +1,9 @@
+
+
+# See Mandrill API: https://mandrillapp.com/api/docs/
+
+# This stub should be able to receive /messages/send.json calls and should store the messages as files on the file system
+
+# There should also be an endpoint that allows the retrieval of the latest N messages
+
+print "TODO"
